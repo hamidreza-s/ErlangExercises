df593e1342ca615ed71a2635e7436f9010bdbe6d
Hamidreza Soleimani <hamidreza.s@gmail.com>
2014-02-15 00:56:06 +0330

    add chapter 13

--- Ch13_Error_Handling/lib_misc.erl
+++ Ch13_Error_Handling/lib_misc.erl
@@ -9,3 +9,7 @@ on_exit(Pid, Fun) ->
 				{'DOWN', Ref, process, Pid, Why} -> Fun(Why)
 			end
 		end).
+
+keep_alive(Name, Fun) ->
+	register(Name, Pid = spawn(Fun)),
+	on_exit(Pid, fun(_Why) -> keep_alive(Name, Fun) end).
