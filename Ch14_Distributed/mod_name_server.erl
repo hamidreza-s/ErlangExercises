1aef029766fde56e2dd645f5830fe6bb36b88823
Hamidreza Soleimani <hamidreza.s@gmail.com>
2014-02-16 00:22:00 +0330

    add chapter 14

---
+++ Ch14_Distributed/mod_name_server.erl
@@ -0,0 +1,15 @@
+-module(mod_name_server).
+-export([start_me_up/3]).
+
+start_me_up(MM, _ArgsC, _ArgsS) -> loop(MM).
+
+loop(MM) ->
+	receive
+		{chan, MM, {store, K, V}} ->
+			kvs:store(K, V),
+			loop(M);
+		{chan, MM, {lookup, K}} ->
+			MM ! {send, kvs:lookup(K)},
+			loop(MM);
+		{chan_closed, MM} ->
+	end.
